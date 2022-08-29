1a082312c02399e43a7898075cf29a415184de50
Vernon-444 <mt.vernon44@gmail.com>
2022-08-28 23:54:09 -0700

    added case for negative numbers

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+if number < 0:
+    last = number  % -10
+    last = last * -1
+else:
+    last = number % 10
+
 if last > 5:
     print("Last digit of", number, "is", last, "and is greater than 5")
 elif last < 6 and last != 0:
