f78b0a133b6fff47682f4b437ab1f78a1d729f8e
Vernon-444 <mt.vernon44@gmail.com>
2022-08-31 11:54:18 -0700

    fixed typos and added cases for proper grammar

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if (len(sys.argv) - 1) == 0:
         print("0 arguments.")
     else:
-        print((len(sys.argv) - 1), "argument:")
+        if (len(sys.argv) - 1) == 1:
+            print((len(sys.argv) - 1), "argument:")
+        else:
+            print((len(sys.argv) - 1), "arguments:")
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
