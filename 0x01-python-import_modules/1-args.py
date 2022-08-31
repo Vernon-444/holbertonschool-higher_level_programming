009a71cf68689a5a181dfc889856050c9cfbb4c8
Vernon-444 <mt.vernon44@gmail.com>
2022-08-31 11:50:03 -0700

    protection while being imported

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if (len(sys.argv) - 1) == 0:
-    print("0 arguments.")
-else:
-    print((len(sys.argv) - 1), "argument:")
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    if (len(sys.argv) - 1) == 0:
+        print("0 arguments.")
+    else:
+        print((len(sys.argv) - 1), "argument:")
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
