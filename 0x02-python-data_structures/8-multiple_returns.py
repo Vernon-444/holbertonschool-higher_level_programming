7ba0b62696005d13f8f2dd0a4dca73a101272d24
Vernon-444 <mt.vernon44@gmail.com>
2022-09-01 01:49:42 -0700

    fixed shebang

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        return (0, None)
+        return ("", None)
     return (len(sentence), sentence[0])
