8bfdf75951c0a7604ce6e1592ae105bf1a97b2c4
Vernon-444 <mt.vernon44@gmail.com>
2022-09-06 12:56:56 -0700

    Testing edge case of 1

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,10 @@
 
 
 def search_replace(my_list, search, replace):
-    if my_list:
+    if len(my_list) > 0:
         new = my_list.copy()
         for i in range(0, len(my_list) - 1):
             if new[i] == search:
                 new[i] = replace
         return new
-    return(my_list)
+    return None
