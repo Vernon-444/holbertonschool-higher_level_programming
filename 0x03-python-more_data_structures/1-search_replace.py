acd32ff63f2a54b6822cbe73539c61e252d50c29
Vernon-444 <mt.vernon44@gmail.com>
2022-09-06 15:02:38 -0700

    added edge case protec to 1

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-
 def search_replace(my_list, search, replace):
-    if len(my_list) > 0:
-        new = my_list.copy()
-        for i in range(0, len(my_list) - 1):
-            if new[i] == search:
-                new[i] = replace
-        return new
-    return None
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy
