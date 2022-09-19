21babddff77f0a45d83a0060d96cd07de44658ee
Vernon-444 <mt.vernon44@gmail.com>
2022-09-19 09:16:05 -0700

    fixed documentation error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,10 @@
 
 
 def write_file(filename="", text=""):
+    """ This writes string to given file,
+    will create file if it does not exist
+    Return the number of characters written"""
+
     with open(filename, "w", encoding="utf-8") as f:
         f.write("{}".format(text))
         f.close
