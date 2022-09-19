1669550186c6a5a7de96c41a6a446e83f1f7b302
Vernon-444 <mt.vernon44@gmail.com>
2022-09-19 09:12:38 -0700

    fixed pep8 violation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Writes a string to text file (UTF8) and returns num of chars written"""
 
+
 def write_file(filename="", text=""):
     with open(filename, "w") as f:
         f.write("{}".format(text))
