cdc24caa1a94bdb388aed474629c572f50d8f133
Vernon-444 <mt.vernon44@gmail.com>
2022-09-19 10:19:11 -0700

    added forgotten notation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,8 @@
 Returns the number of chars added"""
 
 def append_write(filename="", text=""):
+    """Append text to filename and return number of chars added"""
+    
     with open(filename, "a", encoding="utf-8") as f:
         f.write("{}".format(text))
         f.close
