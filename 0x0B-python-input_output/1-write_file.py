1dae78c0785351df4cf6b295d49cc393e902005d
Vernon-444 <mt.vernon44@gmail.com>
2022-09-19 09:11:56 -0700

    this school is cool!

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Writes a string to text file (UTF8) and returns num of chars written"""
+
+def write_file(filename="", text=""):
+    with open(filename, "w") as f:
+        f.write("{}".format(text))
+        f.close
+    return (len(text))
