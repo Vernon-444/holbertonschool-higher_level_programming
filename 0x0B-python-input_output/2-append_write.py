79511ca3934701ef332963684b54f3d19308f85d
Vernon-444 <mt.vernon44@gmail.com>
2022-09-19 10:18:22 -0700

    first add 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Appends string at the end of .txt
+Returns the number of chars added"""
+
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write("{}".format(text))
+        f.close
+    return (len(text))
