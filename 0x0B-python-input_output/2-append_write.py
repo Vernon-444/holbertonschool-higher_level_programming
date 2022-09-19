4e26bec87704d9918ae6d42130fe61f4ce6651b9
Vernon-444 <mt.vernon44@gmail.com>
2022-09-19 10:21:11 -0700

    fixed pep8 violation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,9 +2,10 @@
 """Appends string at the end of .txt
 Returns the number of chars added"""
 
+
 def append_write(filename="", text=""):
     """Append text to filename and return number of chars added"""
-    
+
     with open(filename, "a", encoding="utf-8") as f:
         f.write("{}".format(text))
         f.close
