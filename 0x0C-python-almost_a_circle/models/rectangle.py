89c0b5f95149deb01172a9e198cf25c36a5c5359
Vernon-444 <mt.vernon44@gmail.com>
2022-09-22 18:14:01 -0700

    testing 1

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Class for Rectangle"""
+from models.base import Base
+
+class Rectangle(Base):
+    """Represent a Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Containes specs of the rectangle"""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+        @property
+        def width(self):
+            """Get the width of the Rectangle"""
+            return self.__width
+
+        @property
+        def height(self):
+            """Get the height of the Rectangle"""
+            return self.__height
+
