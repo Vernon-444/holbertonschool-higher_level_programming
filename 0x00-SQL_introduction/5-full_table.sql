e684b0906eb4348565cdb9dd5b418107d8419ee7
Vernon-444 <mt.vernon44@gmail.com>
2022-10-05 14:00:44 -0700

    init comit of the rest of the project

---
+++ 0x00-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints full descrtiption of first_table from
+-- database hbtn_0c_0 in MySQL server
+SHOW CREATE TABLE `first_table`;
---
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- inserts a new row in first_table
+INSERT INTO first_table (`id`, `name`) VALUESA (89, "Best School");
