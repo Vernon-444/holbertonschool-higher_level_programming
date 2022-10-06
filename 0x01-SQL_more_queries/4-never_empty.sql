d6b5b75ff3d1435aa1425254d05155ca5dfd0441
Vernon-444 <mt.vernon44@gmail.com>
2022-10-06 15:57:28 -0700

    pushing more tasks

--- 0x01-SQL_more_queries/4-never_empty.sql
+++ 0x01-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- Creates the table id_not_null
-CREATE TABLE IF NOT EXISTS `id_not_null` (
-    `id`   INT          DEFAULT 1,
-    `name` VARCHAR(256)
+-- Creates a table id_not_null, id has default value of 1
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT(1),
+    name VARCHAR(256)
 );
