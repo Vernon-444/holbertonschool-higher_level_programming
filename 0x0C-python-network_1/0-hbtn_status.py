f5e5389deea0a4cc23463e0ae06c4813ef37bce6
Vernon-444 <mt.vernon44@gmail.com>
2022-11-01 11:25:57 -0700

    upload 0

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+
+"""Fetches status of Holberton Intranet"""
 
 if __name__ == "__main__":
-    pass
+    from urllib import request
+
+    req = request.Request('https://intranet.hbtn.io/status')
+    with request.urlopen(req) as page:
+        page = page.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode('utf8')))
