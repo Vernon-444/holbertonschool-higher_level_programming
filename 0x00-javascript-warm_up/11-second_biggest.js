cf01362dde43141f1a40730962c58fef625f8662
Vernon-444 <mt.vernon44@gmail.com>
2022-12-01 19:35:26 -0800

    upload rest of project

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const numArray = [];
+for (let i = 2; i < process.argv.length; i++) {
+  numArray.push(parseInt(process.argv[i]));
+}
+
+if (numArray.length < 2) {
+  console.log(0);
+} else {
+  let biggest = Number.MIN_VALUE;
+  let secondBiggest = Number.MIN_VALUE;
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] > biggest) {
+      secondBiggest = biggest;
+      biggest = numArray[i];
+    } else if (numArray[i] > secondBiggest && numArray[i] !== biggest) {
+      secondBiggest = numArray[i];
+    }
+  }
+  console.log(secondBiggest);
+}
