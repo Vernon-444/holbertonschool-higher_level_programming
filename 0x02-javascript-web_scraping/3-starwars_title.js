e9d3c2e46f143167f5a83037c262daf2883b0eea
Vernon-444 <mt.vernon44@gmail.com>
2022-12-06 16:13:29 -0800

    adding task 3

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const request = require('request');
+const endpoint = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
+request(endpoint, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
